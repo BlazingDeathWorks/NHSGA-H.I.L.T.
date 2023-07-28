0894bace1fac14c2e4fac43a983fd423d9e084f4
Future Inspire Games <shunsei.chan@gmail.com>
2023-07-28 13:01:32 -0400

    Update PlayerAttackState.cs

--- Assets/Scripts/FSM/States/PlayerAttackState.cs
+++ Assets/Scripts/FSM/States/PlayerAttackState.cs
@@ -6,11 +6,10 @@ public abstract class PlayerAttackState : State
 {
     //Make sure this is sealed on sub abstract class
     protected abstract State TransitionState { get; }
-    private AnimatorStateInfo animatorStateInfo;
 
     protected PlayerAttackState(PlayerEntity playerEntity, FiniteStateMachine finiteStateMachine) : base(playerEntity, finiteStateMachine)
     {
-        animatorStateInfo = PlayerEntity.Animator.GetCurrentAnimatorStateInfo(0);
+        
     }
 
     public override void OnUpdate()
