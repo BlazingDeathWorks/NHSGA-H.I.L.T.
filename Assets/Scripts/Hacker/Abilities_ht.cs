89c75b4621e51e73302e89ce333b28489d64d277
Future Inspire Games <shunsei.chan@gmail.com>
2023-08-07 11:57:29 -0400

    Abilities Slightly Started
    But I should prob do the other upgrades first

--- Assets/Scripts/Hacker/Abilities_ht.cs
+++ Assets/Scripts/Hacker/Abilities_ht.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class Abilities_ht : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public KeyCode Input => input;
+    [SerializeField] private KeyCode input;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
