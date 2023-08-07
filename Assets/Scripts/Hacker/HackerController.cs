e62d234ea99347fa53b648f2e07c6f0802ffe1c4
hrngh <josjamperc@gmail.com>
2023-08-07 10:07:07 -0400

    no aberration for ide

--- Assets/Scripts/Hacker/HackerController.cs
+++ Assets/Scripts/Hacker/HackerController.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class HackerController : MonoBehaviour
 {
     [SerializeField] private KeyCode hackerInterfaceOpenInput = KeyCode.E;
     [SerializeField] private GameObject hackerIDE;
     [SerializeField] private GameObject miniMap;
+    [SerializeField] private Volume volume;
 
+    private ChromaticAberration chromAb;
+    private void Start()
+    {
+        volume.profile.TryGet(out chromAb);
+    }
     private void Update()
     {
         if (Input.GetKeyDown(hackerInterfaceOpenInput))
         {
             hackerIDE.SetActive(!hackerIDE.activeInHierarchy);
             miniMap.SetActive(!hackerIDE.activeInHierarchy);
+            chromAb.active = !chromAb.active;
         }
     }
 }
